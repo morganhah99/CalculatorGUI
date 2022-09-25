d288b695a192a49b9edd70b1e091ad8897902620
morganhah <morganhah99@gmail.com>
2022-09-25 16:51:41 -0400

    added basic arithmetic functionalities, changed colors of buttons

--- calculator.py
+++ calculator.py
@@ -1,54 +1,299 @@
+# Python program to create a simple GUI
+# calculator using Tkinter
+
+# import everything from tkinter module
 from tkinter import *
-from tkinter import ttk
-from tkinter.tix import COLUMN
 
-root = Tk()
-root.title("Calculator GUI")
-
-e = Entry(root, width=35, borderwidth=5)
-
-e.grid(row=0, column = 0, columnspan=3, padx=10, pady=10)
-
-def button_add():
-    return
-
-#button definitions
-
-button_1 = Button(root, text="1", padx=40, pady=20, command=button_add)
-button_2 = Button(root, text="2", padx=40, pady=20, command=button_add)
-button_3 = Button(root, text="3", padx=40, pady=20, command=button_add)
-button_4 = Button(root, text="4", padx=40, pady=20, command=button_add)
-button_5 = Button(root, text="5", padx=40, pady=20, command=button_add)
-button_6 = Button(root, text="6", padx=40, pady=20, command=button_add)
-button_7 = Button(root, text="7", padx=40, pady=20, command=button_add)
-button_8 = Button(root, text="8", padx=40, pady=20, command=button_add)
-button_9 = Button(root, text="9", padx=40, pady=20, command=button_add)
-button_0 = Button(root, text="0", padx=40, pady=20, command=button_add)
-button_addi = Button(root, text="+", padx=39, pady=20, command=button_add)
-button_equal = Button(root, text="=", padx=91, pady=20, command=button_add)
-button_clear = Button(root, text="C", padx=40, pady=20, command=button_add)
-
-#display buttons
-button_1.grid(row=3, column=0)
-button_2.grid(row=3, column=1)
-button_3.grid(row=3, column=2)
+# globally declare the expression variable
+expression = ""
 
 
-button_4.grid(row=2, column=0)
-button_5.grid(row=2, column=1)
-button_6.grid(row=2, column=2)
+# Function to update expression
+# in the text entry box
+def press(num):
+    # point out the global expression variable
+    global expression
 
-button_7.grid(row=1, column=0)
-button_8.grid(row=5, column=2)
-button_9.grid(row=1, column=2)
+    # concatenation of string
+    expression = expression + str(num)
 
-button_0.grid(row=4, column=0)
-button_addi.grid(row=5, column=0)
-button_equal.grid(row=5, column=1, columnspan=2)
-button_clear.grid(row=1, column=1)
-
+    # update the expression by using set method
+    equation.set(expression)
 
 
-root.mainloop()
+# Function to evaluate the final expression
+def equalpress():
+    # Try and except statement is used
+    # for handling the errors like zero
+    # division error etc.
+
+    # Put that code inside the try block
+    # which may generate the error
+    try:
+
+        global expression
+
+        # eval function evaluate the expression
+        # and str function convert the result
+        # into string
+        total = str(eval(expression))
+
+        equation.set(total)
+
+        # initialize the expression variable
+        # by empty string
+        expression = ""
+
+    # if error is generate then handle
+    # by the except block
+    except:
+
+        equation.set(" error ")
+        expression = ""
 
 
+# Function to clear the contents
+# of text entry box
+def clear():
+    global expression
+    expression = ""
+    equation.set("")
+
+
+def delete():
+    global expression
+    expression = ""
+    gui.delete(-1)
+
+
+# Driver code
+if __name__ == "__main__":
+    # create a GUI window
+    gui = Tk()
+
+    # set the background colour of GUI window
+    gui.configure(background="blue")
+
+    # set the title of GUI window
+    gui.title("Simple Calculator")
+
+    # set the configuration of GUI window
+    gui.geometry("270x150")
+
+    def delete_button():
+        gui.delete(0, -1)
+
+    # StringVar() is the variable class
+    # we create an instance of this class
+    equation = StringVar()
+
+    # create the text entry box for
+    # showing the expression .
+    expression_field = Entry(gui, textvariable=equation)
+
+    # grid method is used for placing
+    # the widgets at respective positions
+    # in table like structure .
+    expression_field.grid(columnspan=4, ipadx=70)
+
+    # create a Buttons and place at a particular
+    # location inside the root window .
+    # when user press the button, the command or
+    # function affiliated to that button is executed .
+    button1 = Button(
+        gui,
+        text=" 1 ",
+        fg="black",
+        bg="orange",
+        command=lambda: press(1),
+        height=1,
+        width=7,
+    )
+    button1.grid(row=5, column=0)
+
+    button2 = Button(
+        gui,
+        text=" 2 ",
+        fg="black",
+        bg="orange",
+        command=lambda: press(2),
+        height=1,
+        width=7,
+    )
+    button2.grid(row=5, column=1)
+
+    button3 = Button(
+        gui,
+        text=" 3 ",
+        fg="black",
+        bg="orange",
+        command=lambda: press(3),
+        height=1,
+        width=7,
+    )
+    button3.grid(row=5, column=2)
+
+    button4 = Button(
+        gui,
+        text=" 4 ",
+        fg="black",
+        bg="orange",
+        command=lambda: press(4),
+        height=1,
+        width=7,
+    )
+    button4.grid(row=4, column=0)
+
+    button5 = Button(
+        gui,
+        text=" 5 ",
+        fg="black",
+        bg="orange",
+        command=lambda: press(5),
+        height=1,
+        width=7,
+    )
+    button5.grid(row=4, column=1)
+
+    button6 = Button(
+        gui,
+        text=" 6 ",
+        fg="black",
+        bg="orange",
+        command=lambda: press(6),
+        height=1,
+        width=7,
+    )
+    button6.grid(row=4, column=2)
+
+    button7 = Button(
+        gui,
+        text=" 7 ",
+        fg="black",
+        bg="orange",
+        command=lambda: press(7),
+        height=1,
+        width=7,
+    )
+    button7.grid(row=3, column=0)
+
+    button8 = Button(
+        gui,
+        text=" 8 ",
+        fg="black",
+        bg="orange",
+        command=lambda: press(8),
+        height=1,
+        width=7,
+    )
+    button8.grid(row=3, column=1)
+
+    button9 = Button(
+        gui,
+        text=" 9 ",
+        fg="black",
+        bg="orange",
+        command=lambda: press(9),
+        height=1,
+        width=7,
+    )
+    button9.grid(row=3, column=2)
+
+    button0 = Button(
+        gui,
+        text=" 0 ",
+        fg="black",
+        bg="orange",
+        command=lambda: press(0),
+        height=1,
+        width=7,
+    )
+    button0.grid(row=6, column=1)
+
+    plus = Button(
+        gui,
+        text=" + ",
+        fg="black",
+        bg="orange",
+        command=lambda: press("+"),
+        height=1,
+        width=7,
+    )
+    plus.grid(row=2, column=3)
+
+    minus = Button(
+        gui,
+        text=" - ",
+        fg="black",
+        bg="orange",
+        command=lambda: press("-"),
+        height=1,
+        width=7,
+    )
+    minus.grid(row=3, column=3)
+
+    multiply = Button(
+        gui,
+        text=" * ",
+        fg="black",
+        bg="orange",
+        command=lambda: press("*"),
+        height=1,
+        width=7,
+    )
+    multiply.grid(row=4, column=3)
+
+    divide = Button(
+        gui,
+        text=" / ",
+        fg="black",
+        bg="orange",
+        command=lambda: press("/"),
+        height=1,
+        width=7,
+    )
+    divide.grid(row=5, column=3)
+
+    power = Button(
+        gui,
+        text=" x^2 ",
+        fg="black",
+        bg="orange",
+        command=lambda: press(pow),
+        height=1,
+        width=7,
+    )
+    power.grid(row=2, column=0)
+
+    delete = Button(
+        gui,
+        text=" < ",
+        fg="black",
+        bg="orange",
+        height=1,
+        width=7,
+    )
+    delete.grid(row=2, column=1)
+
+    equal = Button(
+        gui, text=" = ", fg="black", bg="orange", command=equalpress, height=1, width=7
+    )
+    equal.grid(row=6, column=3)
+
+    clear = Button(
+        gui, text="C", fg="black", bg="orange", command=clear, height=1, width=7
+    )
+    clear.grid(row=2, column=2)
+
+    Decimal = Button(
+        gui,
+        text=".",
+        fg="black",
+        bg="orange",
+        command=lambda: press("."),
+        height=1,
+        width=7,
+    )
+    Decimal.grid(row=6, column=2)
+    # start the GUI
+    gui.mainloop()
